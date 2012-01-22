c9548b4af83668927e24a02c741c60c010582daa
Samuel Huckins <shuckins@B74kb0x.(none)>
2012-01-22 12:00:53 -0600

    * Clarified output sections a bit

--- sysadmin/mysql/join-tester.sql
+++ sysadmin/mysql/join-tester.sql
@@ -6,9 +6,11 @@ select * from world.City ci, world.Country co
 where co.code = ci.countrycode
 and GNP < 1000000.0;
 
+select "";
 show warnings;
 
 #------------------------------------------------------------------------------
+select "----------------------------------" as "";
 
 select "JOIN" as "Using version";
 
@@ -18,4 +20,5 @@ select * from world.City ci
 join world.Country co on (co.code = ci.countrycode)
 where GNP < 1000000.0;
 
+select "";
 show warnings;
