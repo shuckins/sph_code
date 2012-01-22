31e323eeb9eb94f6cc6bded5f264a462a4fdb475
Samuel Huckins <shuckins@B74kb0x.(none)>
2012-01-22 10:10:47 -0600

    * New script to verify sameness of comma join syntax versus inner join

---
+++ sysadmin/mysql/join-tester.sql
@@ -0,0 +1,21 @@
+select "COMMA" as "Using version";
+
+# Comma join version
+explain extended 
+select * from world.City ci, world.Country co
+where co.code = ci.countrycode
+and GNP < 1000000.0;
+
+show warnings;
+
+#------------------------------------------------------------------------------
+
+select "JOIN" as "Using version";
+
+# JOIN syntax version
+explain extended
+select * from world.City ci
+join world.Country co on (co.code = ci.countrycode)
+where GNP < 1000000.0;
+
+show warnings;
